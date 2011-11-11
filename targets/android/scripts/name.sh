c3bf72247c1db7a5989d48631b1c8824197dfc5b
Alexis Laferrière <alexis.laf@xymus.net>
2011-11-11 10:56:39 -0500

    android: removes extra prints
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

--- targets/android/scripts/name.sh
+++ targets/android/scripts/name.sh
@@ -4,7 +4,5 @@ version=$3.`date +%s`
 version_code=$3.`date +%s`
 
 sed "s/APP_NAME/$app_name/" ${MNIT_DIR}/targets/android/templates/res/values/strings.xml > res/values/strings.xml
-echo sed "s/APP_NAME/$app_name/" ${MNIT_DIR}/targets/android/templates/res/values/strings.xml \> res/values/strings.xml
 sed -e "s/APP_NAME/$app_name/" -e "s/PACKAGE/$package/" -e "s/VERSION/$version/" -e "s/VERSION_CODE/$version_code/" ${MNIT_DIR}/targets/android/templates/AndroidManifest.xml > AndroidManifest.xml
-echo sed -e "s/APP_NAME/$app_name/" -e "s/PACKAGE/$package/" -e "s/VERSION/$version/" -e "s/VERSION_CODE/$version_code/" ${MNIT_DIR}/targets/android/templates/AndroidManifest.xml \> AndroidManifest.xml
 
